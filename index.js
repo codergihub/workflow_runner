352474fbc834cc8b330195aebd0f36e894979252
webapis <webapis.github@gmail.com>
2021-10-05 13:43:06 +0500

    v

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+var exec = require('child_process').exec
+debugger;
+const packages='dotenv dotenv-expand'
+var cmd = exec(`npm i ${packages}`, function(err, stdout, stderr) {
+    if (err) {
+        debugger;
+      // handle error
+    } 
+    else{
+
+        debugger;
+    
+    }
+    console.log(stdout);
+  });
+  
 console.log("index.js run....",process.env.parameters)
