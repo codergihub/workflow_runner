f20435add54a86e5ae547e97f3015a343812467f
webapis <60083568+webapis@users.noreply.github.com>
2022-03-15 10:54:10 +0500

    1.12.20

--- runRepo.js
+++ runRepo.js
@@ -6,7 +6,6 @@ const makeDir = require('make-dir');
 const pather = require('path')
 const fbDatabase = fbRest().setIdToken(process.env.idToken).setProjectUri(process.env.projectUrl)
 var exec = require('child_process').exec
-var execFile = require('child_process').execFileSync
 async function runRepo({ workflow, workflowEmitter }) {
 
     const { screenName,
@@ -56,15 +55,15 @@ async function runRepo({ workflow, workflowEmitter }) {
     console.log('dependencies....', dependencies)
     //npm i ${dependencies}
     //process.env.LOCAL === 'true' ? `echo 'local dev....'` : 
-    var cmd = exec(process.env.LOCAL === 'true' ? `echo 'local dev....'` : `npm install ${dependencies}`)//, async function (err, stdout, stderr) {
+    var cmd = exec(process.env.LOCAL === 'true' ? `echo 'local dev....'` : `npm install ${dependencies}`, async function (err, stdout, stderr) {
 
-        // console.log('stderr', stderr)
-       // if (err) {
+       // console.log('stderr', stderr)
+        if (err) {
 
             // handle error
-       //     console.log('dependencies not installed', err)
-     //   }
-       // else {
+            console.log('dependencies not installed', err)
+        }
+        else {
 
             //4.RUN WORKFLOW ENTRY FILE
             console.log('dependencies installed')
@@ -78,45 +77,50 @@ async function runRepo({ workflow, workflowEmitter }) {
 
             //run main nodejs
             const main = `${process.cwd()}/${repoName}/main.js`
-            execFile(main)
-            // const worker = new Worker(main, { workerData: {} });
-            // worker.once("message", result => {
-            //     console.log(`${number}th Fibonacci No: ${result}`);
-            // });
 
-            // worker.on("error", async (error) => {
-            //     const currentDate = Date.now()
-            //     const updateWsLogRef = `workspaceLogs/${process.env.selectedWorkspace}/logs/${process.env.wfrunid}/failed`
-            //     const updateTaskLogRef = `taskLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${process.env.taskId}/log/failed`
-            //     const updateWfResultLogRef = `workflowLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${taskId}/workflows/${workflowKey}/log/result`
-            //     const updateWfEndLogRef = `workflowLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${taskId}/workflows/${workflowKey}/log/end`
-            //     //update workflow lastRun
-            //     const updateWfLastLogEnd = `workflows/workspaces/${process.env.selectedWorkspace}/tasks/${taskId}/${workflowKey}/lastLog/end`
-            //     const updateWfLastLogResult = `workflows/workspaces/${process.env.selectedWorkspace}/tasks/${taskId}/${workflowKey}/lastLog/result`
-            //     //update lastRun workspace
-            //     const updateWsLastLogTotalTasks = `workspaces/${process.env.selectedWorkspace}/lastLog/failed`
-            //     //update lastRun task
-            //     const updateTaskLastLogTotalTasks = `workspaces/${process.env.selectedWorkspace}/tasks/${taskId}/lastLog/failed`
-            //     const update = {
-            //         [updateWfResultLogRef]: 'failed',
-            //         [updateWfLastLogResult]: 'failed',
-            //         [updateWfEndLogRef]: currentDate,
-            //         [updateWfLastLogEnd]: currentDate,
-            //         [updateWsLogRef]: { '.sv': { 'increment': 1 } },
-            //         [updateTaskLogRef]: { '.sv': { 'increment': 1 } },
-            //         [updateWsLastLogTotalTasks]: { '.sv': { 'increment': 1 } },
-            //         [updateTaskLastLogTotalTasks]: { '.sv': { 'increment': 1 } }
-            //     }
-            //     console.log(`workflow run error ${error}`);
-            //     const firebaseurl = `${process.env.projectUrl}/.json?auth=${process.env.idToken}`
-            //     await fetch(firebaseurl, { method: 'PATCH', body: JSON.stringify(update) })
-               
+            const worker = new Worker(main, { workerData: {} });
+            worker.once("message", result => {
+                console.log(`${number}th Fibonacci No: ${result}`);
+            });
 
+            worker.on("error", error => {
+                const currentDate = Date.now()
+                const updateWsLogRef = `workspaceLogs/${process.env.selectedWorkspace}/logs/${process.env.wfrunid}/failed`
+                const updateTaskLogRef = `taskLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${process.env.taskId}/log/failed`
+                const updateWfResultLogRef = `workflowLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${taskId}/workflows/${workflowKey}/log/result`
+                const updateWfEndLogRef = `workflowLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${taskId}/workflows/${workflowKey}/log/end`
+                //update workflow lastRun
+                const updateWfLastLogEnd = `workflows/workspaces/${process.env.selectedWorkspace}/tasks/${taskId}/${workflowKey}/lastLog/end`
+                const updateWfLastLogResult = `workflows/workspaces/${process.env.selectedWorkspace}/tasks/${taskId}/${workflowKey}/lastLog/result`
+                //update lastRun workspace
+                const updateWsLastLogTotalTasks = `workspaces/${process.env.selectedWorkspace}/lastLog/failed`
+                //update lastRun task
+                const updateTaskLastLogTotalTasks = `workspaces/${process.env.selectedWorkspace}/tasks/${taskId}/lastLog/failed`
+                const update = {
+                    [updateWfResultLogRef]: 'failed',
+                    [updateWfLastLogResult]: 'failed',
+                    [updateWfEndLogRef]: currentDate,
+                    [updateWfLastLogEnd]: currentDate,
+                    [updateWsLogRef]: { '.sv': { 'increment': 1 } },
+                    [updateTaskLogRef]: { '.sv': { 'increment': 1 } },
+                    [updateWsLastLogTotalTasks]: { '.sv': { 'increment': 1 } },
+                    [updateTaskLastLogTotalTasks]: { '.sv': { 'increment': 1 } }
+                }
+                console.log(`workflow run error ${error}`);
+                fbDatabase.ref('/').update(update, async (error, response) => {
+                    if (!error) {
+                       setTimeout(()=>{
+                        workflowEmitter.emit("WORKFLOW_RUN_FAILED", { taskId, workflowKey })
+                       },5000)
+                     
+                    } else {
+                        console.log('firebase error', error)
+                    }
+                })
+            });
 
-            // });
-
-          //  worker.on("exit", async () => {
-             //   const currentDate = Date.now()
+            worker.on("exit", exitCode => {
+                const currentDate = Date.now()
                 const updateWsLogRef = `workspaceLogs/${process.env.selectedWorkspace}/logs/${process.env.wfrunid}/success`
                 const updateTaskLogRef = `taskLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${process.env.taskId}/log/success`
                 const updateWfResultLogRef = `workflowLogs/${process.env.selectedWorkspace}/${process.env.wfrunid}/tasks/${taskId}/workflows/${workflowKey}/log/result`
@@ -139,96 +143,89 @@ async function runRepo({ workflow, workflowEmitter }) {
                     [updateWsLastLogTotalTasks]: { '.sv': { 'increment': 1 } },
                     [updateTaskLastLogTotalTasks]: { '.sv': { 'increment': 1 } }
                 }
-                const firebaseurl = `${process.env.projectUrl}/.json?auth=${process.env.idToken}`
-                await fetch(firebaseurl, { method: 'PATCH', body: JSON.stringify(update) })
+                debugger;
+                fbDatabase.ref('/').update(update, async (error, response) => {
+                    if (!error) {
+                        debugger;
+                   setTimeout(()=>{
+                    workflowEmitter.emit("WORKFLOW_RUN_SUCCESSFUL", { taskId, workflowKey })
+                   },5000)
+                        
+                    } else {
+                        debugger;
+                        console.log('firebase error', error)
+                    }
 
-        
-              //  setInterval(() => { }, 5000)
+                })
 
-            
-           // });
-
-
-        //}
-        //runRepo
-    
-//})
-//cmd.on('close',()=>{
- 
-  //  setTimeout(()=>{
-        console.log('execute exited')
-        workflowEmitter.emit("WORKFLOW_RUN_SUCCESSFUL", { taskId, workflowKey })
-   // },5000)
-
-//})
-
-// cmd.on('error',(error)=>{
-
-//     setTimeout(()=>{
-//         console.log('execute error',error)
-//         workflowEmitter.emit("WORKFLOW_RUN_FAILED", { taskId, workflowKey })
-//     },5000)
- 
-// })
-
-}
-
-        async function getContentsFromWorkflowRepo({ owner, repoName, tree, token }) {
-
-            const getContent = async function ({ path }) {
-                const fetchPath = `https://api.github.com/repos/${owner}/${repoName}/contents/${path}`
-
-                const response = await fetch(fetchPath, { method: 'GET', headers: { Accept: "application/vnd.github.v3+json", authorization: `token ${token}` } })
-                const data = await response.json()
-
-                return data;
-            }
-
-            const withoutTypeTree = tree.filter(f => f.type !== 'tree')
-            const contents = []
-            for (let t of withoutTypeTree) {
-                const content = await getContent({ path: t.path })
-
-                contents.push(content)
-            }
-
-            return contents
-        }
-
-        async function getWorkflowSourceCodeTree({ owner, repoName, token, selectedBranch }) {
-
-            // Retrieve source code for project
-            //Retrieved source code will be copied to project branch of forked agregators repo
-            //---- List branches endpoint----
-            /*required for the next endoint*/
-            const fetchPath = `https://api.github.com/repos/${owner}/${repoName}/branches`
-
-            const response = await fetch(fetchPath, { method: 'GET', headers: { Accept: "application/vnd.github.v3+json", authorization: `token ${token}` } })
-            const data = await response.json()
-
-            const mainSha = data.find(d => d.name === selectedBranch)
-            const { commit: { sha } } = mainSha
-
-            //------Git database / Get a tree endpoint------
-            /*required to retrieve list of file and folder into*/
-            const treeResponse = await fetch(`https://api.github.com/repos/${owner}/${repoName}/git/trees/${sha}?recursive=1`, { method: 'GET', headers: { Accept: "application/vnd.github.v3+json", authorization: `token ${token}` } })
-            const treeData = await treeResponse.json()
-            const { tree } = treeData
-
-            return tree
-        }
-
-        async function triggerAction({ ticket, body, gh_action_url }) {
-            await fetch(gh_action_url, {
-                method: 'post',
-                headers: {
-                    authorization: `token ${ticket}`,
-                    Accept: 'application/vnd.github.v3+json'
-                },
-                body
             })
 
+
+            setInterval(() => { }, 5000)
         }
+     //   console.log(stdout);
+    });
 
 
-        module.exports = { runRepo, triggerAction }
+
+}//runRepo
+
+
+
+async function getContentsFromWorkflowRepo({ owner, repoName, tree, token }) {
+
+    const getContent = async function ({ path }) {
+        const fetchPath = `https://api.github.com/repos/${owner}/${repoName}/contents/${path}`
+
+        const response = await fetch(fetchPath, { method: 'GET', headers: { Accept: "application/vnd.github.v3+json", authorization: `token ${token}` } })
+        const data = await response.json()
+
+        return data;
+    }
+
+    const withoutTypeTree = tree.filter(f => f.type !== 'tree')
+    const contents = []
+    for (let t of withoutTypeTree) {
+        const content = await getContent({ path: t.path })
+
+        contents.push(content)
+    }
+
+    return contents
+}
+
+async function getWorkflowSourceCodeTree({ owner, repoName, token, selectedBranch }) {
+
+    // Retrieve source code for project
+    //Retrieved source code will be copied to project branch of forked agregators repo
+    //---- List branches endpoint----
+    /*required for the next endoint*/
+    const fetchPath = `https://api.github.com/repos/${owner}/${repoName}/branches`
+
+    const response = await fetch(fetchPath, { method: 'GET', headers: { Accept: "application/vnd.github.v3+json", authorization: `token ${token}` } })
+    const data = await response.json()
+
+    const mainSha = data.find(d => d.name === selectedBranch)
+    const { commit: { sha } } = mainSha
+
+    //------Git database / Get a tree endpoint------
+    /*required to retrieve list of file and folder into*/
+    const treeResponse = await fetch(`https://api.github.com/repos/${owner}/${repoName}/git/trees/${sha}?recursive=1`, { method: 'GET', headers: { Accept: "application/vnd.github.v3+json", authorization: `token ${token}` } })
+    const treeData = await treeResponse.json()
+    const { tree } = treeData
+
+    return tree
+}
+
+async function triggerAction({ ticket, body, gh_action_url }) {
+    await fetch(gh_action_url, {
+        method: 'post',
+        headers: {
+            authorization: `token ${ticket}`,
+            Accept: 'application/vnd.github.v3+json'
+        },
+        body
+    })
+
+}
+module.exports = { runRepo, triggerAction }
